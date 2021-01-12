16b5fb7c4d872bb8e4bf5fd2ee7b74f8d45525fc
Megaterion <77326037+Megaterion@users.noreply.github.com>
2021-01-12 12:33:30 +0100

    Update client.py

--- Chess/client.py
+++ Chess/client.py
@@ -1,3 +1,8 @@
+#==========================================
+# Title:  Schach Client
+# Author: Megaterion
+#==========================================
+
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 import tkinter

d33bff76a7603d4b13fd352ab0f444eb1394efb0
Megaterion <77326037+Megaterion@users.noreply.github.com>
2021-01-13 07:50:39 +0100

    Update spaceinvaders_main.py

--- spaceinvaders_main.py
+++ spaceinvaders_main.py
@@ -14,21 +14,21 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Invaders")
 
 #Gegner laden
-RED_SPACE_SHIP = pygame.image.load(os.path.join("pixel_ship_red_small.png"))
-GREEN_SPACE_SHIP = pygame.image.load(os.path.join("pixel_ship_green_small.png"))
-BLUE_SPACE_SHIP = pygame.image.load(os.path.join("pixel_ship_blue_small.png"))
+RED_SPACE_SHIP = pygame.image.load(os.path.join("SpaceInvaders_Assets/pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("SpaceInvaders_Assets/pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("SpaceInvaders_Assets/pixel_ship_blue_small.png"))
 
 #Spieler laden
-YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("pixel_ship_yellow.png"))
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("SpaceInvaders_Assets/pixel_ship_yellow.png"))
 
 #Laser laden
-RED_LASER = pygame.image.load(os.path.join("pixel_laser_red.png"))
-GREEN_LASER = pygame.image.load(os.path.join("pixel_laser_green.png"))
-BLUE_LASER = pygame.image.load(os.path.join("pixel_laser_blue.png"))
-YELLOW_LASER = pygame.image.load(os.path.join("pixel_laser_yellow.png"))
+RED_LASER = pygame.image.load(os.path.join("SpaceInvaders_Assets/pixel_laser_red.png"))
+GREEN_LASER = pygame.image.load(os.path.join("SpaceInvaders_Assets/pixel_laser_green.png"))
+BLUE_LASER = pygame.image.load(os.path.join("SpaceInvaders_Assets/pixel_laser_blue.png"))
+YELLOW_LASER = pygame.image.load(os.path.join("SpaceInvaders_Assets/pixel_laser_yellow.png"))
 
 #Hintergrund laden
-BG = pygame.transform.scale(pygame.image.load(os.path.join("background-black.png")), (WIDTH, HEIGHT))
+BG = pygame.transform.scale(pygame.image.load(os.path.join("SpaceInvaders_Assets/background-black.png")), (WIDTH, HEIGHT))
 
 class Laser:
     def __init__(self, x, y, img):

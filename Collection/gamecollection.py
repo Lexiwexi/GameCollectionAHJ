a59b5a3f494a8be8e89161a9782ccc2e5b8bae2b
Megaterion <77326037+Megaterion@users.noreply.github.com>
2021-01-18 10:59:09 +0100

    Update gamecollection.py

--- Collection/gamecollection.py
+++ Collection/gamecollection.py
@@ -30,10 +30,13 @@ def win0(event=None):
     clearwin()
     chess_start_button = tkinter.Button(mframe, command=win1, text="Schach")
     spaceInvaders_start_button = tkinter.Button(mframe, command=win2, text="Space Invaders")
+    tictactoe_button = tkinter.Button(mframe, command=win3, text="Tic-Tac-Toe")
     quit_button = tkinter.Button(bottomframe, command=close_window, text="Quit")
     
     chess_start_button.pack(fill=tkinter.X)
     spaceInvaders_start_button.pack(fill=tkinter.X)
+    tictactoe_start_button.pack(fill=tkinter.X)
+
     quit_button.pack()
 
 def win1(event=None):
@@ -59,9 +62,21 @@ def win2(event=None):
     back = tkinter.Button(mframe, command=win0, text='Back')
     back.pack()
     
+def win3(event=None):
+    #Tic-Tac-Toe
+    clearwin()
+    label1 = tkinter.Label(mframe, text='Tic-Tac-Toe starten?')
+    label1.pack()
+    start = tkinter.Button(mframe, command=start_tictactoe, text='Start')
+    start.pack()
+    back = tkinter.Button(mframe, command=win0, text='Back')
+    back.pack()
+    
 def start_space_Invaders():
     runpy.run_path('spaceinvaders_main.py')
 
+def start_tictactoe():
+    runpy.run_path('tictactoe_main.py')
     
 def close_window():
     main.destroy()

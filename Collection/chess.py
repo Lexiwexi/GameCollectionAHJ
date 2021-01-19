5026e1613723b1252ad2d73bde09d24364ef8936
Alex <77324389+Lexiwexi@users.noreply.github.com>
2021-01-19 11:00:21 +0100

    Update chess.py
    Comments

--- Collection/chess.py
+++ Collection/chess.py
@@ -8,21 +8,31 @@ charset = "ABCDEFGH"
 playerList = ["Black","White"]
 
 
+#*~*- Documentation of important stuffs -*~*
+#aboard -> array of board
+#aboard[y-position][x-position][color,piece]
+
+
 def linSearch(lys, element):
+    #Linear Search Algorithm
     for i in range (len(lys)):
         if lys[i] == element:
             return i
     return -1
 
-# Generate/Reset board
+
 def boardGen():
+    # Generate/Reset board through appending
+    # new list elements
     for i in range(8):
         aboard.append([])
         for j in range(8):
             aboard[i].append([0,0])
 
-#Setup Board with pieces
+
 def boardSet():
+    # Setup Board with pieces by asigning
+    # each color-square a piece
     for i in range(2):
         for j in range(8):
             aboard[i][j][0] = 1
@@ -55,8 +65,10 @@ def boardSet():
     aboard[-1][-1][1] = 5
 
 
-## Display board in console line by line
+
 def boardDisplay():
+    # Display board in console line by line
+    # Function is redundant in pygame
     display = ""
     print("x",charset,"x")
     for i in range(len(aboard)):
@@ -97,8 +109,11 @@ def boardDisplay():
         print()
     print("x",charset,"x")
 
-#is move x1 y1 to x2 y2 legal?
+
 def isLegalMove(x1,y1,x2,y2,piece,currentcolor):
+    # Check if move from 'x1','y1' to 'x2','y2' with
+    # piece 'piece' with current player color as
+    # 'currentcolor' is legal
     c = 1
     if currentcolor != 0:
         if currentcolor == 1:
@@ -106,27 +121,30 @@ def isLegalMove(x1,y1,x2,y2,piece,currentcolor):
         if currentcolor == 2:
             c = -1
 
+    loc1    = aboard[y1][x1]
+    loc2    = aboard[y2][x2] 
+    if (loc1[0] == currentcolor) and (loc2[0] != currentcolor):
         
-        #print(aboard[y1][x1][0])
-        if ((aboard[y1][x1][0] == currentcolor) and (aboard[y1][x1][1] == piece) ) and (aboard[y2][x2][0] != currentcolor):
-            if piece == 0: #Pawn
-                if ( (y1 != 1) and (c==1) ) or ( (y1 != 6) and (c==-1) ):
-                    if ( ( (x2 == x1) and (y2 == y1+(1*c)) ) and ( aboard[y2][x2] == [0,0] ) ) or ( ( ( (x2==x1+1) or (x2==x1-1) ) and (y2 == y1+1*c) ) and ( aboard[y2][x2] != [0,0] ) ):
-                        return True
-                else:
-                    if ( ( (x2 == x1) and (y2 == y1+(2*c) ) and ( aboard[y2][x2] == [0,0] ) ) or ( ( ( (x2==x1+1) or (x2==x1-1) ) and (y2 == y1+2*c) ) and ( aboard[y2][x2] != [0,0] ) ) ) or ( ( ( (x2 == x1) and (y2 == y1+(1*c)) ) and ( aboard[y2][x2] == [0,0] ) ) or ( ( ( (x2==x1+1) or (x2==x1-1) ) and (y2 == y1+1*c) ) and ( aboard[y2][x2] != [0,0] ) ) ):
-                        print("hell yea")
-                        return True
+        if loc1[1] == 0: #if piece is Pawn
+            if ( (y1 != 1) and (c==1) ) or ( (y1 != 6) and (c==-1) ): #if not in Pawn-Line* (x = 2 for black, x = 7 for white)
+                if ( ( (x2 == x1) and (y2 == y1+(1*c)) ) and ( aboard[y2][x2] == [0,0] ) ) or ( ( ( (x2==x1+1) or (x2==x1-1) ) and (y2 == y1+1*c) ) and ( aboard[y2][x2] != [0,0] ) ):
+                    #ifselected square empty and no movement in x position and movement in y position only 1 (or -1 in case of white) 
+                    #or if selected square enemy color and 
+                    return True
+            else:
+                if ( ( (x2 == x1) and (y2 == y1+(2*c) ) and ( aboard[y2][x2] == [0,0] ) ) or ( ( ( (x2==x1+1) or (x2==x1-1) ) and (y2 == y1+2*c) ) and ( aboard[y2][x2] != [0,0] ) ) ) or ( ( ( (x2 == x1) and (y2 == y1+(1*c)) ) and ( aboard[y2][x2] == [0,0] ) ) or ( ( ( (x2==x1+1) or (x2==x1-1) ) and (y2 == y1+1*c) ) and ( aboard[y2][x2] != [0,0] ) ) ):
+                    print("hell yea")
+                    return True
 
-            if piece == 1: #Knight
+            if loc1[1] == 1: #if piece is Knight
                 if ( (x2 == x1+2)or(x2 == x1-2) ) and ( (y2 == y1+1*c) or (y2 == y1-1*c) ) or ( (x2 == x1+1)or(x2 == x1-1) ) and ( (y2 == y1+2*c) or (y2 == y1-2*c) ):
                     return True
 
-            if piece == 2: #King
+            if loc1[1] == 2: #if piece is King
                 if  ( ( (x2==x1+1)or(x2==x1-1) ) or ( (y2==y1+1*c)or(y2==y1-1*c) ) )  or ( ( (x2==x1+1)or(x2==x1-1) ) and ( (y2==y1+1*c)or(y2==y1-1*c) ) ):
                     return True
 
-            if piece == 3: #Queen
+            if piece == 3: #if piece is Queen
                 if ( (x2-x1)==(y2-y1) ):
                     return True
 
@@ -142,39 +160,37 @@ def isLegalMove(x1,y1,x2,y2,piece,currentcolor):
                             return False
                     return True
 
-            if piece == 4:
+            if loc1[1] == 4: #if piece is Bishop
                 if (x2-x1)==(y2-y1):
                     for n in range( int( (float(x2-x1)**0.5)**2) ):
                         print(n,'n')
                     return True
 
-            if piece == 5:
-                if ( ( (x2-x1) == 0) and ( (y2-y1) != 0) ):
-                    #print(y2-y1*c,"in y")
-                    for n in range( ( (y2-y1)*c )-1 ):
-                        #print(n)
-                        #print(aboard[y1+n+1][x1])
-                        if aboard[y1+n+1][x1] != [0,0]:
-                            return False
+            if loc1[1] == 5: #if piece is Rook
 
+                if ( ( (x2-x1) == 0) and ( (y2-y1) != 0) ): #if only movement in y pos
+                    for n in range( ( (y2-y1)*c )-1 ): #look between y1 and y2 to see if they are full
+                        if aboard[y1+n+1][x1] != [0,0]: #if so, return False
+                            return False
                     return True
-                if ( ( (x2-x1) != 0) and ( (y2-y1) == 0) ):
-                    #print(x2-x1)
-                    for n in range(x2-x1-1):
-                        #print(n)
-                        if aboard[y1][x1+n+1] != [0,0]:
+                
+                if ( ( (x2-x1) != 0) and ( (y2-y1) == 0) ): #if only movement in x pos
+                    for n in range(x2-x1-1): #look between x1 and x2 to see if they are full
+                        if aboard[y1][x1+n+1] != [0,0]: #if so, return False
                             return False
                     return True
 
 
 
-#move piece from x1,y1 to x2,y2
+
 def boardUpdate(x1,y1,x2,y2):
+    # move piece from 'x1','y1' to 'x2','y2'
     aboard[y2][x2]=aboard[y1][x1]
     aboard[y1][x1]=[0,0]
 
-#Output piece Name
+
 def pieceId(array):
+    # Output piece Name; Redundant in pygame
     rank = array[0]
     color = array[1]
     outp = ""
@@ -200,6 +216,7 @@ def pieceId(array):
     return(outp)
 
 def playerId(player):
+    # Output color of player
     if player == 1:
         return "Black"
     if player == 2:
@@ -207,8 +224,9 @@ def playerId(player):
     else:
         return "???"
 
-#PlayerTurn
+
 def playerMove(Player):
+    # Get player input and output into array
     z = False
 
     print("Player",playerList[Player-1],"'s turn:   ")
@@ -271,7 +289,6 @@ while G:
     boardDisplay()
     roundCount += 1
     move = playerMove(Player) #x1,y1,x2,y2
-    #print(aboard[move[1]][move[0]][1])
     if isLegalMove(move[0],move[1],move[2],move[3],aboard[move[1]][move[0]][1],Player):
         boardUpdate(move[0],move[1],move[2],move[3])
         print(pieceId([aboard[move[1]-1][move[0]-1][1] , Player]), "moves to", (charset[move[2]]+str(move[3]) ) )
